c39ff43333526795ad770847fb7f5289ce20bc29
jamesbersalunaaa <jamesbersalunaa@gmail.com>
2026-05-29 01:52:18 +0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -6,6 +6,9 @@ apt-get install -y php8.2 php8.2-cli php8.2-mbstring php8.2-xml php8.2-curl php8
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install --no-dev --optimize-autoloader
 
+npm install
+npm run build
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
